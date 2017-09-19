69af1ed9637421d50f619c173e1ae640e4d09d76
domas <domantas@nectarsun.com>
2017-09-19 21:58:49 +0300

    adds implementation for magnets

---
+++ magnets.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+#include "rtos.h"
+
+#define RELAY_ON 0
+#define RELAY_OFF 1
+
+// relays are active low
+BusOut magnet_relays(PH_1, PH_0, PC_15, PC_14);
+
+void turnOn(uint8_t relay) {
+  magnet_relays &= ~(0x1 << (relay - 1));
+}
+
+void turnOff(uint8_t relay) {
+  magnet_relays |= 0x1 << (relay - 1);
+}
+
+namespace magnets {
+  void setup() {
+    // turn off all relays
+    magnet_relays = 0xFFF;
+  }
+}
---
+++ magnets.h
@@ -0,0 +1,8 @@
+#ifndef MAGNETS_H
+#define MAGNETS_H
+
+namespace magnets {
+  void setup();
+}
+
+#endif
