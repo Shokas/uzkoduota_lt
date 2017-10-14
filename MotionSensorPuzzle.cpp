a79ddd784e1296777bb8fc0ca27abc0da3eb4b8c
paplaukias <domantas.cibas@gmail.com>
2017-10-14 12:59:12 +0300

    updates MotionSensorPuzzle

--- MotionSensorPuzzle.cpp
+++ MotionSensorPuzzle.cpp
@@ -1,19 +1,21 @@
 #include "MotionSensorPuzzle.h"
+#include "MaskPuzzle.h"
 
+Puzzle *mask_puzzle = new MaskPuzzle();
 InterruptIn motion_sensor(PB_8);  //  active low
 
 void MotionSensorPuzzle::activate() {
+  mask_puzzle->activate();
   motion_sensor.fall(callback(this, &MotionSensorPuzzle::motion_triggered));
   activated = true;
 }
 
 void MotionSensorPuzzle::motion_triggered() {
+  mask_puzzle->solve();
+  // open drawer
   startCooldown();
 }
 
 // activates masks 5mins coolown, open drawer for 10-15s, 2 mins cooldown
 void MotionSensorPuzzle::solve() {
-  if(isActive() && !coolingDown()) {
-    
-  }
 }
