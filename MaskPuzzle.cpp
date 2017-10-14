1bbecd7e0e3184540feebd0427ef31d642ae4cee
paplaukias <domantas.cibas@gmail.com>
2017-10-14 11:28:02 +0300

    updates MaskPuzzle

--- MaskPuzzle.cpp
+++ MaskPuzzle.cpp
@@ -1,20 +1,23 @@
 #include "MaskPuzzle.h"
-#include "mbed.h"
 
+uint8_t repeat = 2;
 BusOut masks(PA_10, PB_3, PB_5, PB_4, PB_10, PA_8, PA_9, PC_7, PB_6, PA_7);
 
 void blinkPattern() {
   // blink pattern in here
-  int i;
-  for(i=0; i <= 10; i++) {
-    masks = 0x001 << i;
-    wait(1);
+  for(int r=0; r<repeat; r++) {
+    for(int i=0; i<=10; i++) {
+      masks = 0x001 << i;
+      // add one second delay
+    }
+    // add 10 second delay
   }
 }
 
 void MaskPuzzle::solve() {
-  if(this->isActive()) {
-    blinkPattern();
+  if(isActive() && !coolingDown()) {
     printf("Mask Puzzle started");
+    blinkPattern();
+    startCooldown();
   }
 }
