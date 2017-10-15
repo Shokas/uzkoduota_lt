354b595defe0d78556aef37558326e9efdddab53
paplaukias <domantas.cibas@gmail.com>
2017-10-15 15:09:27 +0300

    updates NfcReader solve method

--- NfcPuzzle.cpp
+++ NfcPuzzle.cpp
@@ -2,15 +2,19 @@
 #include "MFRC522.h"
 
 // #define MF_RESET    D8
-MFRC522 nfcItem(PC_12, PC_11, PC_10, PD_2, PC_9);
+MFRC522 nfcReader(PC_12, PC_11, PC_10, PD_2, PC_9);
 
 void NfcPuzzle::activate() {
+  printf("NFC Puzzle activated\r\n");
   activated = true;
 }
 
 void NfcPuzzle::solve() {
   if(isActive() && !coolingDown()) {
-    // do stuff
-    
+    if(nfcReader.PICC_IsNewCardPresent()) {
+      printf("NFC Puzzle solved\r\n");
+      this->deactivate();
+      // activate distance sensor puzzle
+    }
   }
 }
