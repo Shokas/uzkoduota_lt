a538ebbd8c57841c2fc367883cfba2ad3601c314
paplaukias <domantas.cibas@gmail.com>
2017-10-16 01:14:50 +0300

    updates distance and distance2 to dist and dist2

--- DistanceSensorPuzzle.cpp
+++ DistanceSensorPuzzle.cpp
@@ -8,29 +8,29 @@ HCSR04 sr2(PC_8, PC_6);
 //HCSR04 sr3(PB_15, PB_14);       //PB_14, PB_13
 
 uint8_t sensorNum;
-float distance, distance2;
+float dist, dist2;
 
 void DistanceSensorPuzzle::activate() {
   printf("Distance Sensor Puzzle activated\r\n");
   activated = true;
   sensorNum = 1;
-  distance = 200;
-  distance2 = 200;
+  dist = 200;
+  dist2 = 200;
 }
 
 void DistanceSensorPuzzle::solve() {
   if(isActive() && !coolingDown()) {
     if(sensorNum == 1) {
-      distance = sr.getCm();
+      dist = sr.getCm();
       // if < 100, turn on LED lights
     }
     if(sensorNum == 2) {
-      distance2 = sr2.getCm();
+      dist2 = sr2.getCm();
       // if < 100, turn on LED lights
     }
-    printf("Sensor %d distance %.2f\r\n", sensorNum, distance);
+    printf("Sensor %d distance %.2f\r\n", sensorNum, dist);
 
-    if((distance < 100) && (distance2 < 100)) {
+    if((dist < 100) && (dist2 < 100)) {
       printf("Distance sensors solved\r\n");
       // open drawer
       this->startCooldown();
