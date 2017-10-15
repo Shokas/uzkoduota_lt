b80dd01831e720c9268baf499ea2c214a7960e1d
paplaukias <domantas.cibas@gmail.com>
2017-10-16 01:24:50 +0300

    updates main loop

--- main.cpp
+++ main.cpp
@@ -5,14 +5,9 @@
 #include "MotionSensorPuzzle.h"
 #include "NfcPuzzle.h"
 #include "DistanceSensorPuzzle.h"
-// #include "rtos.h"
+#include "SafePuzzle.h"
 
-DigitalOut led(D13);
-
-// InterruptIn bigButton(PB_13);//(USER_BUTTON); //USER_BUTTON == PC_13
-
-// Ticker distTick;
-Ticker blinky;
+InterruptIn bigButton(USER_BUTTON); //USER_BUTTON == PC_13; BIG_BUTTON(PB_13);//
 
 //magnets PH_1, PH_0, PC_15, PC_14
 //leds PB_7, PA_15, PA_14, PA_13
@@ -23,27 +18,22 @@ Puzzle *painting_puzzle = new PaintingPuzzle();
 Puzzle *motion_puzzle = new MotionSensorPuzzle();
 Puzzle *nfc_puzzle = new NfcPuzzle();
 Puzzle *distance_puzzle = new DistanceSensorPuzzle();
-//SafePuzzle
-
-void blinky_ISR() {
-  led = !led;
-}
+Puzzle *safe_puzzle = new SafePuzzle();
 
 int main() {
   printf("Hello Game\r\n");
-  led = 0;
-  blinky.attach(&blinky_ISR, 1.0);
-  magnets.turnOff(MAG1);  //activate trap door
   painting_puzzle->activate();
-  motion_puzzle->activate();
-  nfc_puzzle->activate();
-  distance_puzzle->activate();
+  safe_puzzle->activate();
+  // motion_puzzle->activate();
+  // nfc_puzzle->activate();
+  // distance_puzzle->activate();
 
-  painting_puzzle->solve();
-  motion_puzzle->solve();
-  // while(1) {
-  //   painting_puzzle->solve();
-  //   motion_sensor_puzzle->solve();
-  //   __WFI();
-  // }
+  while(1) {
+    painting_puzzle->solve();
+    safe_puzzle->solve();
+
+  // motion_puzzle->solve();
+  // motion_sensor_puzzle->solve();
+    __WFI();
+  }
 }
