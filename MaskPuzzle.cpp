6407d248f52c5d9157c16e6043af366b1ec4e33f
paplaukias <domantas.cibas@gmail.com>
2017-10-16 01:19:52 +0300

    adds delays to MaskPuzzle blink pattern

--- MaskPuzzle.cpp
+++ MaskPuzzle.cpp
@@ -1,4 +1,5 @@
 #include "MaskPuzzle.h"
+#include "mbed.h"
 
 uint8_t repeat = 2;
 BusOut masks(PA_10, PB_3, PB_5, PB_4, PB_10, PA_8, PA_9, PC_7, PB_6, PA_7);
@@ -8,9 +9,9 @@ void blinkPattern() {
   for(int r=0; r<repeat; r++) {
     for(int i=0; i<=10; i++) {
       masks = 0x001 << i;
-      // add one second delay
+      wait(1);  // change with non-blocking delay
     }
-    // add 10 second delay
+    wait(10);  // change with non-blocking delay
   }
 }
 
